51c8d9176c3985c61c72a476948e6430937bb446
JoshieV <joshievaccaro@gmail.com>
2026-09-06 20:57:37 +0100

    !slap @membername, Bot sends message + gif on command

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -29,5 +29,14 @@ class Fun(commands.Cog):
         await poll_message.add_reaction("👍")
         await poll_message.add_reaction("👎")
 
+    @commands.command()
+    async def slap(self, ctx, member: discord.Member=None):
+        if member is None:
+            await ctx.send("Mention someone to slap them. !slap @membername")
+            return
+
+        await ctx.send(f"{member.mention} got slapped by {ctx.author.mention}!")
+        await ctx.send("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDV4ZXJzMmRjNmpzZ3d4ZXNpM3dhNzZlMjlhN2g4ejg1ZHEwcGJ2YyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/mEtSQlxqBtWWA/giphy.gif")
+
 async def setup(bot):
     await bot.add_cog(Fun(bot))
